c412729ba9ab97ef36ecec9c9e050bbb83d7baa1
Patrick Edson <pedson@leapscientific.com>
2016-03-31 13:25:52 -0400

    Script cleanup

--- devtools/tempdb.sh
+++ devtools/tempdb.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-docker run --name some-postgres -e POSTGRES_PASSWORD=pgsecret -v $(PWD)/../swc-datastore:/docker-entrypoint-initdb.d --rm -p 5432:5432 postgres
+# First agument is location of the swc-datastore directory.  Not required if running from the root ndb directory.
+
+if [ $# -eq 0 ]; then
+    SRCDIR=$(PWD)
+else
+    SRCDIR=$1
+fi
+
+echo $SRCDIR
+
+docker run --name some-postgres -e POSTGRES_PASSWORD=pgsecret -v $SRCDIR/swc-datastore:/docker-entrypoint-initdb.d --rm -p 5432:5432 postgres
