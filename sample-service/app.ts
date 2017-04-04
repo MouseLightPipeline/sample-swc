2924cd315d23842ca1f2e5861bce124457148ac8
Patrick Edson <pedson@leapscientific.com>
2017-04-04 10:31:37 -0400

    Enable typescript and yarn

--- sample-service/app.ts
+++ sample-service/app.ts
@@ -1,26 +1,37 @@
 "use strict";
+
 const SwaggerExpress = require("swagger-express-mw");
 const SwaggerUi = require("swagger-tools/middleware/swagger-ui");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const compress = require("compression");
 const methodOverride = require("method-override");
 const logger = require("morgan");
+
 const db = require("./api/models/index");
+
 const app = express();
+
 const http = require("http");
 const server = http.Server(app);
 const io = require("socket.io")(server);
+
 const debug = require("debug")("ndb:sample-service:server");
+
 app.locals.dbready = false;
+
 io.on("connection", () => {
     broadcastAll();
 });
+
 module.exports.app = app; // for testing
 module.exports.broadcast = broadcastAll;
+
 const config = {
-    appRoot: __dirname // required config
+    appRoot: __dirname  // required config
 };
+
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -28,18 +39,26 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(compress());
 app.use(methodOverride());
+
 app.use("/script/socket.io", express.static(__dirname + "/node_modules/socket.io-client"));
+
 SwaggerExpress.create(config, (err, swaggerExpress) => {
     if (err) {
         throw err;
     }
+
     app.use(SwaggerUi(swaggerExpress.runner.swagger));
+
     // install middleware
     swaggerExpress.register(app);
+
     const port = process.env.PORT || 9641;
+
     server.listen(port);
 });
+
 sync();
+
 function sync() {
     syncDatabase().then(() => {
         return broadcastAll();
@@ -51,6 +70,7 @@ function sync() {
         setTimeout(sync, 5000);
     });
 }
+
 function syncDatabase() {
     return new Promise((resolve, reject) => {
         db.sequelize.authenticate().then(() => {
@@ -58,12 +78,14 @@ function syncDatabase() {
             resolve(db);
         }).catch((err) => {
             reject(err);
-        });
+        })
     });
 }
+
 function broadcastAll() {
     return new Promise((resolve) => {
         io.emit("db_status", app.locals.dbready);
+
         if (app.locals.dbready) {
             db.Sample.count().then(function (val) {
                 io.emit("sampleCount", val);
@@ -74,6 +96,7 @@ function broadcastAll() {
             db.RegistrationTransform.count().then(function (val) {
                 io.emit("registrationTransformCount", val);
             });
+
             db.Injection.count().then(function (val) {
                 io.emit("injectionCount", val);
             });
@@ -83,6 +106,7 @@ function broadcastAll() {
             db.Fluorophore.count().then(function (val) {
                 io.emit("fluorophoreCount", val);
             });
+
             db.Neuron.count().then(function (val) {
                 io.emit("neuronCount", val);
             });
@@ -90,8 +114,7 @@ function broadcastAll() {
                 io.emit("brainAreaCount", val);
             });
             resolve(true);
-        }
-        else {
+        } else {
             io.emit("sampleCount", -1);
             io.emit("mouseStrainCount", -1);
             io.emit("registrationTransformCount", -1);
@@ -103,6 +126,4 @@ function broadcastAll() {
             resolve(false);
         }
     });
-}
-;
-//# sourceMappingURL=app.js.map
+};
