5a0f45d519923e78a0deae2fd3518bc78001dd49
Patrick Edson <pedson@leapscientific.com>
2017-04-18 08:32:31 -0400

    Remove deprecated services.

--- dockerhub.sh
+++ dockerhub.sh
@@ -2,8 +2,8 @@
 
 if [ "$#" -gt 0 ] &&  [ "$1" = "push" ]; then
     echo "pushing tagged docker containers to hub"
-    cd sample-service && npm run docker-push && cd ../sample-web && npm run docker-push && cd ../swc-service && npm run docker-push && cd ../swc-web && npm run docker-push
+    cd sample-service && npm run docker-push && cd ../sample-web && npm run docker-push
 else
     echo "building tagged docker containers"
-    cd sample-service && npm run docker-build && cd ../sample-web && npm run docker-build && cd ../swc-service && npm run docker-build && cd ../swc-web && npm run docker-build
+    cd sample-service && npm run docker-build && cd ../sample-web && npm run docker-build
 fi
